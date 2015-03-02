690649880d688b5862c2f748b6b9fa60dc67101f
Emanuele Tamponi <emanuele.tamponi@gmail.com>
2015-03-02 11:47:17 +0100

    Move string to const

--- deltaphi/__init__.py
+++ deltaphi/__init__.py
@@ -3,5 +3,8 @@ import os
 __author__ = 'Emanuele Tamponi'
 
 
+TEST_RESOURCES = "test_resources/"
+
+
 def test_file_path(test_file):
-    return os.path.join(os.path.dirname(__file__), "test_resources/", test_file)
+    return os.path.join(os.path.dirname(__file__), TEST_RESOURCES, test_file)
