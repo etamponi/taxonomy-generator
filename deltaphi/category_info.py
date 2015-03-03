9910a5a49e64d96dafb58425b387b7e10419f5c2
Emanuele Tamponi <emanuele.tamponi@gmail.com>
2015-03-03 10:19:29 +0100

    Implement CategoryInfo using hierarchy

--- deltaphi/category_info.py
+++ deltaphi/category_info.py
@@ -9,15 +9,23 @@ class TermsError(Exception):
 
 class CategoryInfo(object):
 
-    def __init__(self, category, documents, terms, frequencies):
+    def __init__(self, category, documents, terms, frequencies, children):
         self.category = category
         self.documents = documents
         self.terms = terms
         self.frequencies = numpy.asarray(frequencies)
-        self.children = set()
+        self.children = children
 
-    @classmethod
-    def merge(cls, *category_infos):
+
+class SingleCategoryInfo(CategoryInfo):
+
+    def __init__(self, category, documents, terms, frequencies):
+        super(SingleCategoryInfo, self).__init__(category, documents, terms, frequencies, set())
+
+
+class MultipleCategoryInfo(CategoryInfo):
+
+    def __init__(self, *category_infos):
         if any(ci.terms is not category_infos[0].terms for ci in category_infos):
             raise TermsError()
         categories = sorted(ci.category for ci in category_infos)
@@ -25,6 +33,6 @@ class CategoryInfo(object):
         merged_documents = sum(ci.documents for ci in category_infos)
         merged_frequencies = sum(ci.frequencies for ci in category_infos)
         merged_children = set(category_infos)
-        merged = CategoryInfo(merged_category, merged_documents, category_infos[0].terms, merged_frequencies)
-        merged.children = merged_children
-        return merged
+        super(MultipleCategoryInfo, self).__init__(
+            merged_category, merged_documents, category_infos[0].terms, merged_frequencies, merged_children
+        )
--- deltaphi/test_category_info.py
+++ deltaphi/test_category_info.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy
-from deltaphi.category_info import CategoryInfo
+from deltaphi.category_info import SingleCategoryInfo, MultipleCategoryInfo
 
 __author__ = 'Emanuele Tamponi'
 
@@ -8,7 +8,7 @@ __author__ = 'Emanuele Tamponi'
 class TestCategoryInfo(unittest.TestCase):
 
     def test_init(self):
-        ci = CategoryInfo("Name", 100, ["a", "b", "c"], [10, 30, 80])
+        ci = SingleCategoryInfo("Name", 100, ["a", "b", "c"], [10, 30, 80])
         self.assertIsInstance(ci.frequencies, numpy.ndarray)
         self.assertEqual("Name", ci.category)
         self.assertEqual(100, ci.documents)
@@ -17,9 +17,9 @@ class TestCategoryInfo(unittest.TestCase):
 
     def test_pairwise_merge(self):
         terms = ["a", "b", "c"]
-        ci1 = CategoryInfo("C1", 100, terms, [50, 0, 80])
-        ci2 = CategoryInfo("C2", 80, terms, [0, 40, 20])
-        merged = CategoryInfo.merge(ci1, ci2)
+        ci1 = SingleCategoryInfo("C1", 100, terms, [50, 0, 80])
+        ci2 = SingleCategoryInfo("C2", 80, terms, [0, 40, 20])
+        merged = MultipleCategoryInfo(ci1, ci2)
         numpy.testing.assert_array_equal([50, 40, 100], merged.frequencies)
         self.assertEqual("(C1+C2)", merged.category)
         self.assertEqual(180, merged.documents)
@@ -27,10 +27,10 @@ class TestCategoryInfo(unittest.TestCase):
 
     def test_multiple_merge(self):
         terms = ["a", "b", "c"]
-        ci1 = CategoryInfo("C1", 100, terms, [50, 0, 80])
-        ci2 = CategoryInfo("C2", 80, terms, [0, 40, 20])
-        ci3 = CategoryInfo("C3", 130, terms, [20, 20, 30])
-        merged = CategoryInfo.merge(ci1, ci2, ci3)
+        ci1 = SingleCategoryInfo("C1", 100, terms, [50, 0, 80])
+        ci2 = SingleCategoryInfo("C2", 80, terms, [0, 40, 20])
+        ci3 = SingleCategoryInfo("C3", 130, terms, [20, 20, 30])
+        merged = MultipleCategoryInfo(ci1, ci2, ci3)
         numpy.testing.assert_array_equal([70, 60, 130], merged.frequencies)
         self.assertEqual("(C1+C2+C3)", merged.category)
         self.assertEqual(310, merged.documents)
@@ -38,11 +38,11 @@ class TestCategoryInfo(unittest.TestCase):
 
     def test_hierarchical_merge(self):
         terms = ["a", "b", "c"]
-        ci1 = CategoryInfo("C1", 100, terms, [50, 0, 80])
-        ci2 = CategoryInfo("C2", 80, terms, [0, 40, 20])
-        ci3 = CategoryInfo("C3", 130, terms, [20, 20, 30])
-        middle = CategoryInfo.merge(ci1, ci2)
-        merged = CategoryInfo.merge(middle, ci3)
+        ci1 = SingleCategoryInfo("C1", 100, terms, [50, 0, 80])
+        ci2 = SingleCategoryInfo("C2", 80, terms, [0, 40, 20])
+        ci3 = SingleCategoryInfo("C3", 130, terms, [20, 20, 30])
+        middle = MultipleCategoryInfo(ci1, ci2)
+        merged = MultipleCategoryInfo(middle, ci3)
         numpy.testing.assert_array_equal([70, 60, 130], merged.frequencies)
         self.assertEqual("((C1+C2)+C3)", merged.category)
         self.assertEqual(310, merged.documents)
