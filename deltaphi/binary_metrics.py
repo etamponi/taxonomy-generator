466dec647025fbf67dad134eb05439d59dac396c
Emanuele Tamponi <emanuele.tamponi@gmail.com>
2015-03-04 11:41:59 +0100

    Implement and test BinaryCharacteristic

--- deltaphi/binary_metrics.py
+++ deltaphi/binary_metrics.py
@@ -11,3 +11,9 @@ class BinaryDiscriminant(Metric):
 
     def evaluate(self, ci1, ci2):
         return ci1.frequencies / ci1.documents - ci2.frequencies / ci2.documents
+
+
+class BinaryCharacteristic(Metric):
+
+    def evaluate(self, ci1, ci2):
+        return ci1.frequencies / ci1.documents + ci2.frequencies / ci2.documents - 1.0
--- deltaphi/test_binary_metrics.py
+++ deltaphi/test_binary_metrics.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy
-from deltaphi.binary_metrics import BinaryDiscriminant
+from deltaphi.binary_metrics import BinaryDiscriminant, BinaryCharacteristic
 from deltaphi.category_info import SingleCategoryInfo
 
 __author__ = 'Emanuele Tamponi'
@@ -17,3 +17,8 @@ class TestBinaryMetrics(unittest.TestCase):
         d = BinaryDiscriminant()
         expected_discriminant = numpy.asarray([60. / 100., -30. / 80., (80. / 100.) - (50. / 80.)])
         numpy.testing.assert_array_equal(expected_discriminant, d.evaluate(self.ci1, self.ci2))
+
+    def test_characteristic(self):
+        c = BinaryCharacteristic()
+        expected_characteristic = numpy.asarray([(60. / 100.) - (80. / 80.), -50. / 80., (80. / 100.) - (30. / 80.)])
+        numpy.testing.assert_array_equal(expected_characteristic, c.evaluate(self.ci1, self.ci2))
