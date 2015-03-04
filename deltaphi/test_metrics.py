68768c4f4a0f7d066253bc6af134b95e33f78b23
Emanuele Tamponi <emanuele.tamponi@gmail.com>
2015-03-04 11:46:03 +0100

    Rename module test_binary_metrics to test_metrics

--- deltaphi/test_metrics.py
+++ deltaphi/test_metrics.py
@@ -6,19 +6,19 @@ from deltaphi.category_info import SingleCategoryInfo
 __author__ = 'Emanuele Tamponi'
 
 
-class TestBinaryMetrics(unittest.TestCase):
+class TestMetrics(unittest.TestCase):
 
     def setUp(self):
         self.terms = ["a", "b", "c"]
         self.ci1 = SingleCategoryInfo("A", 100, self.terms, [60, 0, 80])
         self.ci2 = SingleCategoryInfo("B", 80, self.terms, [0, 30, 50])
 
-    def test_discriminant(self):
+    def test_binary_discriminant(self):
         d = BinaryDiscriminant()
         expected_discriminant = numpy.asarray([60. / 100., -30. / 80., (80. / 100.) - (50. / 80.)])
         numpy.testing.assert_array_equal(expected_discriminant, d.evaluate(self.ci1, self.ci2))
 
-    def test_characteristic(self):
+    def test_binary_characteristic(self):
         c = BinaryCharacteristic()
         expected_characteristic = numpy.asarray([(60. / 100.) - (80. / 80.), -50. / 80., (80. / 100.) - (30. / 80.)])
         numpy.testing.assert_array_equal(expected_characteristic, c.evaluate(self.ci1, self.ci2))
