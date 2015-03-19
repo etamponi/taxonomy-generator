838c51e0f13db1e68a3a70fe501bf18a067a00b2
Emanuele Tamponi <emanuele.tamponi@gmail.com>
2015-03-19 14:06:55 +0100

    Test for lemmatizer

--- deltaphi/test_term_processor.py
+++ deltaphi/test_term_processor.py
@@ -8,7 +8,7 @@ __author__ = 'Emanuele Tamponi'
 
 class TestTermProcessor(unittest.TestCase):
 
-    def test_filter_terms(self):
+    def test_filter_terms_min_length(self):
         original_terms = [u"a", u"aa", u"aaa", u"aaaa"]
         expected_terms_1 = [u"aaa", u"aaaa"]
         expected_terms_2 = [u"aa", u"aaa", u"aaaa"]
@@ -16,3 +16,9 @@ class TestTermProcessor(unittest.TestCase):
         self.assertEqual(expected_terms_1, tp.filter(original_terms))
         tp = TermProcessor(min_length=2)
         self.assertEqual(expected_terms_2, tp.filter(original_terms))
+
+    def test_filter_lemmatize(self):
+        original_terms = [u"testing", u"grouper", u"done", u"loved", u"reported"]
+        expected_terms = [u"test", u"group", u"do", u"love", u"report"]
+        tp = TermProcessor(min_length=1)
+        self.assertEqual(expected_terms, tp.filter(original_terms))
