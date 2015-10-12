86262d31b0cfc35799c1e4f582896c029d7f668e
Emanuele Tamponi <emanuele.tamponi@gmail.com>
2015-10-12 16:05:53 +0200

    More tests and logs

--- deltaphi/experiments/run_experiments.py
+++ deltaphi/experiments/run_experiments.py
@@ -2,10 +2,11 @@ import cPickle
 import csv
 import sys
 
+from deltaphi import shapes
 from deltaphi.category_info import CategoryLayer
 from deltaphi.experiments.assessment import Taxonomy, SearchTaxonomy, TaxonomyScore
 from deltaphi.experiments.dataset_definitions import DATASET_DEFINITIONS
-from deltaphi.metrics import LookAhead, CharacteristicTerms, DiscriminantTerms, Cohesion, Separability
+from deltaphi.metrics import LookAhead, CharacteristicTerms, DiscriminantTerms
 from deltaphi.parent_layer_search import LayerGreedyMergeSearch
 from deltaphi.raw_filter import RawFilter
 from deltaphi.sources import CSVRawSource, CategoryInfoSource
@@ -16,8 +17,8 @@ __author__ = 'Emanuele'
 DATASET_DIR = "datasets"
 RESULT_DIR = "results"
 
-CHARACTERISTIC_AREA = Cohesion.DEFAULT_AREA
-DISCRIMINANT_AREA = Separability.DEFAULT_AREA
+CHARACTERISTIC_AREA = shapes.PSphere([1, 0], 0.6, 1)
+DISCRIMINANT_AREA = shapes.PSphere([0, 1], 1.0, 1) | shapes.PSphere([0, -1], 1.0, 1)
 
 SEARCHER = SearchTaxonomy(
     parent_layer_search=LayerGreedyMergeSearch(
@@ -31,23 +32,25 @@ SEARCHER = SearchTaxonomy(
         )
     )
 )
-SEARCHER_NAME = "default"
+SEARCHER_NAME = "characteristic_0.6_discriminant_1.0"
 
 
 def main():
     csv.field_size_limit(sys.maxint)
-    print "Testing", SEARCHER_NAME
-    for dataset_name in get_dataset_names():
-        print "Running experiment:", dataset_name
-        categories = get_categories(dataset_name, DATASET_DIR)
-        reference, generated, score, layer_scores = run_experiment(SEARCHER, categories)
-        print "Reference taxonomy:"
-        print_taxonomy(reference)
-        print "Generated taxonomy:"
-        print_taxonomy(generated)
-        print "Scores:"
-        print_scores(layer_scores, score)
-        save_results(SEARCHER_NAME, dataset_name, [reference, generated, score, layer_scores], RESULT_DIR)
+    log_path = "{}/{}_log.txt".format(RESULT_DIR, SEARCHER_NAME)
+    with open(log_path, "w") as f:
+        log(f, "Testing {}", SEARCHER_NAME)
+        for dataset_name in get_dataset_names():
+            log(f, "Running experiment: {}", dataset_name)
+            categories = get_categories(dataset_name, DATASET_DIR)
+            reference, generated, score, layer_scores = run_experiment(SEARCHER, categories)
+            log(f, "Reference taxonomy:")
+            log_taxonomy(f, reference)
+            log(f, "Generated taxonomy:")
+            log_taxonomy(f, generated)
+            log(f, "Scores:")
+            log_scores(f, layer_scores, score)
+            save_results(SEARCHER_NAME, dataset_name, [reference, generated, score, layer_scores], RESULT_DIR)
 
 
 def get_dataset_names():
@@ -74,20 +77,26 @@ def run_experiment(searcher, categories):
     return reference, generated, score, layer_scores
 
 
-def print_taxonomy(taxonomy):
+def log_taxonomy(f, taxonomy):
     for i, layer in enumerate(taxonomy.layers):
-        print i, "=", layer
+        log(f, "{} = {}", i, layer)
 
 
-def print_scores(layer_scores, total_score):
-    print "Layer scores:", layer_scores
-    print "Total score: ", total_score
+def log_scores(f, layer_scores, total_score):
+    log(f, "Layer scores: {}", layer_scores)
+    log(f, "Total score: {}", total_score)
 
 
 def save_results(searcher_name, dataset_name, results, result_dir):
     file_path = "{}/{}_{}.dat".format(result_dir, searcher_name, dataset_name)
     with open(file_path, "w") as f:
         cPickle.dump(results, f)
+
+
+def log(f, fmt, *params):
+    line = fmt.format(*params)
+    f.write(line + "\n")
+    print line
 
 
 if __name__ == '__main__':
